170b7a5df3c16216b8e63442eed977f864b5d42f
Pranay92 <pranaydubey3009@gmail.com>
2015-06-24 00:00:05 +0530

    Update quick union demo

--- quick union/index.js
+++ quick union/index.js
@@ -11,8 +11,11 @@ function QuickUnion(len) {
 }
 
 
-QuickUnion.prototype.root = function(i) {
+QuickUnion.prototype.root = function(i,log) {
 	while(i !== this.id[i]) {
+		if(log) {
+			console.log('count');
+		}
 		i = this.id[i]
 	}
 
@@ -30,17 +33,26 @@ QuickUnion.prototype.union = function(p,q) {
 	this.id[i] = j
 };
 
-var qu = new QuickUnion(10);
+var qu = new QuickUnion(100);
 
 qu.union(2,3);
 qu.union(4,5);
 qu.union(2,7);
 qu.union(4,3);
+qu.union(41,3);
+qu.union(14,23);
+qu.union(24,3);
+qu.union(94,43);
+qu.union(12,1);
+qu.union(79,1);
+qu.union(46,2);
+qu.union(6,15);
+qu.union(61,21);
 
 console.log(qu.root(2));
 console.log(qu.root(1));
-console.log(qu.root(3));
+console.log(qu.root(3,true));
 
-console.log(qu.connected(2,4));
-console.log(qu.connected(3,7));
-console.log(qu.connected(1,9));
+// console.log(qu.connected(2,4));
+// console.log(qu.connected(3,7));
+// console.log(qu.connected(1,9));
